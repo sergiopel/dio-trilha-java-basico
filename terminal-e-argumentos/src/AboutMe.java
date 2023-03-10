65cba320bbb2bc9ee694f8a1610bd33139666f97
Sergio Pelegrini <serpel@gmail.com>
2023-03-10 15:19:23 -0300

    Aqui eu aprendi sobre terminal, argumentos e entrada de dados pelo console utilizando a classe Scanner

---
+++ terminal-e-argumentos/src/AboutMe.java
@@ -0,0 +1,26 @@
+/**
+ * <h1>AboutMe</h1>
+ * 
+ * Essa classe demonstra como rodar um programa
+ * esperando argumentos
+ * 
+ * @author   Sergio
+ * @version  1.0
+ * @since    10/03/2023
+ *
+ */
+public class AboutMe {
+    public static void main(String[] args) {
+        //os argumentos começam com indice 0
+        String nome = args [0];
+        
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]); //vamos falar sobre Wrappers
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        
+    }
+}
